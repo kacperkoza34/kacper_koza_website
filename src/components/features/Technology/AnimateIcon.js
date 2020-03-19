ccdf4e1b9420c5d19f187b4e337676efbe1cf0b5
Kacper Koza <kacperkoza34@gmail.com>
2020-03-19 12:46:08 +0100

    Add technology component

--- src/components/features/Technology/AnimateIcon.js
+++ src/components/features/Technology/AnimateIcon.js
@@ -6,19 +6,8 @@ import TrackVisibility from 'react-on-screen';
 
 const AnimateIcon = (props) => {
 
-
-  const start = () => {
-
-
-    gsap.set([...<Icon/>], {autoAlpha: 0});
-    const tl = gsap.timeline({defaults: { ease: 'power3.inOut'}});
-    tl.fromTo([...<Icon/>],{scaleY:0, autoAlpha: 0}, {duration: .5, scaleY:1 ,autoAlpha: 1});
-    return <Icon/>
-  };
   return(
-    <TrackVisibility offset={1000}>
-      {({ isVisible }) => isVisible ? start() : ''}
-    </TrackVisibility>
+    <Icon/>
   );
 };
 

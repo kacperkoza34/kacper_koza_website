33f3dd2ff5e83f2dd5ecede1de4c82598a16dfdb
Kacper Koza <kacperkoza34@gmail.com>
2020-03-19 21:01:54 +0100

    Add nav animation

---
+++ src/components/features/Nav/MobileNav.js
@@ -0,0 +1,48 @@
+import React, { useRef } from 'react';
+import styles from './MobileNav.module.scss';
+import {NavLink} from 'react-router-dom';
+import { navConfig } from './NavConfig';
+import gsap from 'gsap';
+
+class MobileNav extends React.Component {
+  state = {
+    active: false
+  }
+
+  handler(status){
+    this.setState({active:status});
+  }
+
+  displayNav(){
+    const animation = document.querySelector('#test');
+    console.log(animation);
+    const tl = gsap.timeline({defaults: { ease: 'power3.inOut'}});
+    tl.to(animation,{duration: 1 , x: 0, autoAlpha: 1})
+  }
+
+  hideNav(){
+    const animation = document.querySelector('#test');
+    console.log(animation);
+    const tl = gsap.timeline({defaults: { ease: 'power3.inOut'}});
+    tl.to(animation,{duration: 1 , x: 1000, autoAlpha: 1})
+  }
+
+  render(){
+    console.log(this.state.active);
+    return (
+      <div className={styles.mobileNav}>
+        <div onClick={() => this.displayNav() }>x</div>
+
+        <div id={'test'} className={styles.mobileList}>
+          <div className={styles.start}/>
+            <div className={styles.wrapper}>
+              <div onClick={() => this.hideNav() }>xsss</div>
+            </div>
+          <div className={styles.end}/>
+        </div>
+
+      </div>
+    )
+  }
+}
+export default MobileNav;
--- src/components/features/Nav/Nav.js
+++ src/components/features/Nav/Nav.js
@@ -2,27 +2,42 @@ import React from 'react';
 import styles from './Nav.module.scss';
 import {NavLink} from 'react-router-dom';
 import { navConfig } from './NavConfig';
+import MobileNav from './MobileNav';
 
 class Nav extends React.Component {
   state = {
-    mobileNav: false
+    mobile: false
+  }
+
+  componentDidMount(){
+    if(window.innerWidth <= 900) this.setState({mobile:true});
+
+    window.addEventListener('resize', () =>{
+      if(window.innerWidth <= 900) this.setState({mobile:true});
+      else this.setState({mobile:false});
+    })
   }
   render(){
-    if (window.screen.width <= 900) {
-      console.log(window.screen.width)
-    }
     return (
-      <div className={styles.nav}>
-        <ul>
-        {navConfig.map( ({title, link}) =>(
-          <li key={link}>
-            <NavLink to={link}>{title}</NavLink>
-          </li>
-          )
-         )
-        }
-        </ul>
-      </div>
+        <>
+        { !this.state.mobile ?
+
+          <div className={styles.nav}>
+            <ul>
+            {navConfig.map( ({title, link}) =>(
+              <li key={link}>
+                <NavLink to={link}>{title}</NavLink>
+              </li>
+              )
+             )
+            }
+            </ul>
+          </div>
+
+          :
+          <MobileNav/>
+          }
+        </>
     );
   }
 }

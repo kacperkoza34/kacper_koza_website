1aefa7a4d00d9ba99c682d7e12d74b3ba1488b1f
Kacper Koza <kacperkoza34@gmail.com>
2020-03-20 12:11:49 +0100

    Update nav

--- src/components/features/Nav/MobileNav.js
+++ src/components/features/Nav/MobileNav.js
@@ -3,37 +3,42 @@ import styles from './MobileNav.module.scss';
 import {NavLink} from 'react-router-dom';
 import { navConfig } from './NavConfig';
 import gsap from 'gsap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 class MobileNav extends React.Component {
-
+  state = {
+    nav: false,
+  }
   componentDidMount(){
+    this.setState({nav: document.querySelector('#nav')});
     window.addEventListener('resize', () =>{
       this.hideNav();
     })
   }
 
   displayNav(){
-    const animation = document.querySelector('#test');
-    console.log(animation);
     const tl = gsap.timeline({defaults: { ease: 'power3.inOut'}});
-    tl.to(animation,{duration: 1 , x: 0, autoAlpha: 1})
+    tl.to(this.state.nav, {duration: 1 , x: 0, autoAlpha: 1})
   }
 
   hideNav(){
-    const animation = document.querySelector('#test');
-    console.log(animation);
     const tl = gsap.timeline({defaults: { ease: 'power3.inOut'}});
-    tl.to(animation,{duration: 1 , x: 1000, autoAlpha: 1})
+    tl.to(this.state.nav, {duration: 1 , x: 1000, autoAlpha: 1})
   }
 
   render(){
     return (
       <div className={styles.mobileNav}>
-        <div onClick={() => this.displayNav() }>x</div>
-        <div id={'test'} onTouchMove={() => this.hideNav()} className={styles.mobileList}>
+        <div className={styles.burger} onClick={() => this.displayNav() }>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <div id={'nav'} onTouchMove={() => this.hideNav()} className={styles.mobileList}>
           <div className={styles.start}/>
             <div className={styles.wrapper}>
-              <div onClick={() => this.hideNav() }>xsss</div>
+              <div className={styles.burger} onClick={() => this.hideNav() }>
+                <FontAwesomeIcon icon={faArrowRight} />
+              </div>
               {navConfig.map( ({title, link}) =>(
                 <div onClick={() => this.hideNav()} className={styles.singleLink} key={link}>
                   <NavLink to={link}>{title}</NavLink>
